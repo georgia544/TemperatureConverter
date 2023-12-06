7ad424494ee38e4628f362df2c11073bf7a7c5ca
georgia544 <diana.shakh98@gmail.com>
2023-12-06 16:32:53 +0300

    add converter logic and UI

--- app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/hfad/temperatureconverter/MainActivity.kt
@@ -1,27 +1,118 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.hfad.temperatureconverter
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hfad.temperatureconverter.ui.theme.TemperatureConverterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            MaterialTheme {
+                Surface {
 
+                    MainActivityContent()
+                }
+            }
         }
     }
 }
 
+@Composable
+fun MainActivityContent() {
+    val celsius = remember { mutableStateOf(0) }
+    val newCelsius = remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Header(image = R.drawable.sunrise, description = "sunrise image")
+        EnterTemperature(newCelsius.value) { newCelsius.value = it }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+
+            ConvertButton {
+                newCelsius.value.toIntOrNull()?.let {
+                    celsius.value = it
+                }
+            }
+        }
+        TemperatureText(celsius.value)
+    }
+
+}
 
 
+@Preview(showBackground = true)
+@Composable
+fun PreviewMainActivity() {
+    MainActivityContent()
 
+}
+
+
+@Composable
+fun Header(image: Int, description: String) {
+    Image(
+        painter = painterResource(id = image),
+        contentDescription = description,
+        modifier = Modifier
+            .height(180.dp)
+            .fillMaxWidth(),
+        contentScale = ContentScale.Crop
+    )
+}
+
+@Composable
+fun TemperatureText(celsium: Int) {
+    val fahrenheit = (celsium.toDouble() * 9 / 5) + 32
+    Text("$celsium Celsius is $fahrenheit Fahrenheit")
+}
+
+@Composable
+fun ConvertButton(clicked: () -> Unit) {
+    Button(onClick = clicked) {
+        Text("Convert")
+
+    }
+}
+
+@Composable
+fun EnterTemperature(temperature: String, changed: (String) -> Unit) {
+    TextField(
+        value = temperature,
+        label = { Text("Enter a temperature in Celsius") },
+        onValueChange = changed,
+        modifier = Modifier.fillMaxWidth()
+    )
 }
